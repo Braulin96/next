102baaf00852f4ed3ea8a89f241d5f755275b342
Braulin Pires <braulinpires@hotmail.com>
2024-03-06 09:59:32 +0000

    personalized not found page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,6 +1,19 @@
-export default function ReviewDetail ( {params} : {params : {
+import { notFound } from "@/node_modules/next/navigation";
+
+export default function ReviewDetail({
+  params,
+}: {
+  params: {
     productId: string;
     reviewId: string;
-}} ) {
-    return <h1>Review {params.reviewId} for product {params.productId} </h1>
-} 
+  };
+}) {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
+  return (
+    <h1>
+      Review {params.reviewId} for product {params.productId}{" "}
+    </h1>
+  );
+}
