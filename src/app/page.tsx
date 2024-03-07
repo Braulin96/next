a8b9d7fa658bc14665f4d2f5799fad38cd10032a
Braulin Pires <braulinpires@hotmail.com>
2024-03-07 08:54:04 +0000

    routing metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home (){
-    return <h1> Welcome home!</h1>
-}
+const Home = () => {
+  return <div className="flex">
+    <div className="bg-green-900 rounded-2xl px-10 w-10 aspect-square">
+        <p className="text-red-300 bg-green-400">hello</p>
+
+    </div>
+    <div className="bg-blue-800 rounded-2xl px-10 w-10 aspect-square">
+        container 2
+
+    </div>
+  </div>;
+};
+export default Home
+
+// export default function Home() {
+//   return <h1> Welcome home!</h1>;
+// }
