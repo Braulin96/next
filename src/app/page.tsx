b8fdc766bd76e3bde5664af2e82023d39c812170
Braulin Pires <braulinpires@hotmail.com>
2024-03-08 09:54:13 +0000

    link component navigation -tut 20

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
+import Link from "next/link";
+
 const Home = () => {
-  return <div className="flex">
-    <div className="bg-green-900 rounded-2xl px-10 w-10 aspect-square">
-        <p className="text-red-300 bg-green-400">hello</p>
-
-    </div>
-    <div className="text-blue-500 rounded-2xl px-10 w-10 aspect-square">
-        container 2
-
-    </div>
-  </div>;
+  return (
+    <>
+      <h1>Welcome Home</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/products">Products</Link>
+    </>
+  );
 };
-export default Home
+export default Home;
 
 // export default function Home() {
 //   return <h1> Welcome home!</h1>;
