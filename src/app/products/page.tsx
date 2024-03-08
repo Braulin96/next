472d3255091b4adb0703bd7478d3ccb34c628d58
Braulin Pires <braulinpires@hotmail.com>
2024-03-08 10:28:36 +0000

    naviganting programmatically -tut 21

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -7,7 +7,7 @@ export default function ProductList() {
       <h1>Product List</h1>
       <h2><Link href="products/1">Product 1</Link></h2>
       <h2><Link href="products/2">Product 2</Link></h2>
-      <h2><Link href="products/3" replace>Product 3</Link></h2>
+      <h2><Link href="products/3">Product 3</Link></h2>
       <h2><Link href={`products/${productId}`}>Product {productId}</Link></h2>
       <Link href="/" >Home</Link>
     </>
