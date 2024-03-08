89dbec7c35928ffe2acb5f63c1c9b8fb370b6649
Braulin Pires <braulinpires@hotmail.com>
2024-03-08 09:29:13 +0000

    title metadata- tut 18

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: {
+        absolute: "Blog", //this way it will ignore the template propriety in layout and instead of being blog | Code Evolution would be only Blog.
+    }
+}
 export default function Blog () {
     return <h1>My Blog</h1>
 }
